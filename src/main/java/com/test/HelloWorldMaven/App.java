77912f4663d357ba029c82280fb41579779a59a8
devops1406 <37294777+devops1406@users.noreply.github.com>
2020-08-25 21:12:51 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello!!!" );
         System.out.println( "Hello Jenkins Test1!!!" );
+        System.out.println( "Hello Jenkins Test1!!!" );
         
     }
 }
