9bbaba57d7f52a20221121b0582eb7000cd2d9bb
devops1406 <37294777+devops1406@users.noreply.github.com>
2020-08-25 20:36:17 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -6,7 +6,7 @@ package com.test.HelloWorldMaven;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void ( String[] args )
     {
         System.out.println( "Hello Chittu" );
         System.out.println( "Hello Chetan" );
