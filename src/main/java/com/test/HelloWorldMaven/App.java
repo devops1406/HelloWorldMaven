ae5433826835681ac8ceb20ccc1f3f8299b64b8f
devops1406 <37294777+devops1406@users.noreply.github.com>
2020-08-25 21:10:03 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Branch TEST!!!" );
+        System.out.println( "Hello Branch Hello!!!" );
     }
 }
